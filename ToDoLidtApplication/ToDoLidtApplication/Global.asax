a79b8b8fd1ce83daca19af1a9f0fa4acd3aca2d4
Fahimeh Hajari <fahime.hajari@gmail.com>
2021-11-30 00:34:38 -0500

    ToDo project created.

---
+++ ToDoLidtApplication/ToDoLidtApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDoLidtApplication.MvcApplication" Language="C#" %>
---
+++ ToDoLidtApplication/ToDoLidtApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoLidtApplication.Startup))]
+namespace ToDoLidtApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
