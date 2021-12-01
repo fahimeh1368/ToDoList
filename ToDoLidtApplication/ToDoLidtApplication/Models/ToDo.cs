bc5a7fc681f7ed85977035426ceeee73d36ee567
Fahimeh Hajari <fahime.hajari@gmail.com>
2021-12-01 18:26:21 -0500

    ToDo model added.

--- ToDoLidtApplication/ToDoLidtApplication/Models/ToDo.cs
+++ ToDoLidtApplication/ToDoLidtApplication/Models/ToDo.cs
@@ -7,5 +7,10 @@ namespace ToDoLidtApplication.Models
 {
     public class ToDo
     {
+        public int Id { set; get; }
+        public string description { set; get; }
+        public bool IsDone { get; set; }
+        public virtual ApplicationUser User { set; get; }
+
     }
 }
